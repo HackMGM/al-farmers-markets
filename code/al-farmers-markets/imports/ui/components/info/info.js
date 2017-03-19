66917298744cae5bbcb8ed1eeb8ffa62b7a698a6
Elijah Lofgren <elijahlofgren@gmail.com>
2017-03-19 01:06:33 -0500

    Add google map of farmers markets.

--- code/al-farmers-markets/imports/ui/components/info/info.js
+++ code/al-farmers-markets/imports/ui/components/info/info.js
@@ -15,6 +15,42 @@ Template.info.onCreated(function () {
   instance.state = new ReactiveDict();
 });
 
+Template.info.onRendered(function () {
+  console.log('rendered');
+  // Wait 2 sec for page to render.
+  setTimeout(function () {
+    // Adapted from http://stackoverflow.com/a/19640897/908677
+    var map;
+    var elevator;
+    var myOptions = {
+      zoom: 12,
+      center: new google.maps.LatLng(32.3798522, -86.2971825),
+      mapTypeId: 'terrain'
+    };
+    map = new google.maps.Map($('#map_canvas')[0], myOptions);
+
+    $('.address').each(function (index) {
+      var address = $(this).text();
+      var name = $(this).parent().find('.name').text();
+      console.log('address = ' + address);
+      //for (var x = 0; x < addresses.length; x++) {
+      $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=' + address, null, function (data) {
+        console.log('got result:');
+        console.log(data.results);
+        var p = data.results[0].geometry.location
+        var latlng = new google.maps.LatLng(p.lat, p.lng);
+        new google.maps.Marker({
+          position: latlng,
+          map: map,
+          title: name + '\n' + address
+        });
+
+      });
+    });
+
+  }, 2000);
+});
+
 Template.info.events({
   'change .city-filter'(event, instance) {
     instance.state.set('cityFilter', event.target.value);
