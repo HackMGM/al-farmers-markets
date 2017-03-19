4966738142d512544c1d2eddcb955e00514e0edd
Elijah Lofgren <elijahlofgren@gmail.com>
2017-03-19 00:10:56 -0500

    Add the ability to filter to a specific city.

--- code/al-farmers-markets/imports/ui/components/info/info.js
+++ code/al-farmers-markets/imports/ui/components/info/info.js
@@ -1,6 +1,6 @@
-import { Markets } from '/imports/api/markets/markets.js';
-
 import { Meteor } from 'meteor/meteor';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { Markets } from '/imports/api/markets/markets.js';
 
 import '/imports/ui/components/AddMarket/AddMarket.js';
 
@@ -10,13 +10,41 @@ import './info.html';
 window.Markets = Markets;
 
 Template.info.onCreated(function () {
+  var instance = this;
   Meteor.subscribe('markets.all');
-  
+  instance.state = new ReactiveDict();
+});
+
+Template.info.events({
+  'change .city-filter'(event, instance) {
+    instance.state.set('cityFilter', event.target.value);
+  },
 });
 
 Template.info.helpers({
+  cities() {
+    let markets = Markets.find({}).fetch();
+    console.log(markets);
+    let cities = [];
+    for (let i = 0; i < markets.length; i++) {
+      var city = markets[i].address_city;
+      // TODO: Add support for IE < 9 by using libray for this.
+      if (-1 === cities.indexOf(city)) {
+        cities.push(city);
+      }
+    }
+    console.log(cities);
+    return cities;
+  },
   markets() {
-    return Markets.find({});
+    const instance = Template.instance();
+    const cityFilter = instance.state.get('cityFilter');
+    if (cityFilter) {
+      return Markets.find({ address_city: cityFilter });
+    } else {
+      return Markets.find({});
+
+    }
   },
 });
 
